048b586690881343dac614c1631360939d3fcc7b
kirill poznyak <k.paznyak@gmail.com>
2025-02-16 22:19:28 +0300

    complete task return-negative

---
+++ 8kyu/return-negative.js
@@ -0,0 +1,13 @@
+// DISCRIPTION
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// NOTES
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+// SOLUTION
+function makeNegative(num) {
+    // Code?
+    let negativeNum = (num > 0) ? -num : num;
+    return negativeNum;
+}
